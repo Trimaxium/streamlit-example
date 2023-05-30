998e9a0bb109ac250c98d88b23d3aa5135f959e4
Trimaxium <31966278+Trimaxium@users.noreply.github.com>
2023-05-29 22:38:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,87 +15,55 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-class Creature:
-    def __init__(self, level, health, physical_attack, magic_attack, armor, magic_resistance, faith, speed):
-        self.level = level
-        self.health = health
-        self.physical_attack = physical_attack
-        self.magic_attack = magic_attack
-        self.armor = armor
-        self.magic_resistance = magic_resistance
-        self.faith = faith
-        self.speed = speed
+import streamlit as st
+import random
 
-    def calculate_damage(self, move, defender):
-        attack_stat = self.physical_attack if move.type == MoveType.Physical else self.magic_attack
-        defense_stat = defender.armor if move.type == MoveType.Physical else defender.magic_resistance
-        damage = move.power * (attack_stat / defense_stat) * (self.level / defender.level) + 2
-        return int(damage)
-
-    def calculate_healing(self, move):
-        healing = (move.power * self.faith) / ((self.level / 3.0) + 1)
-        return int(healing)
-
-    def attack(self, move, defender):
-        if move.is_healing_move:
-            healing_amount = self.calculate_healing(move)
-            self.health += healing_amount
-            print(f"Creature healed for {healing_amount} health.")
-        else:
-            damage_amount = self.calculate_damage(move, defender)
-            defender.health -= damage_amount
-            print(f"Creature dealt {damage_amount} damage to the opponent.")
-
-
-class Move:
-    def __init__(self, name, power, move_type, is_healing_move):
+class Pokemon:
+    def __init__(self, name, health, attack):
         self.name = name
-        self.power = power
-        self.type = move_type
-        self.is_healing_move = is_healing_move
+        self.health = health
+        self.attack = attack
 
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
 
-class MoveType:
-    Physical = 1
-    Magic = 2
+def battle(pokemon1, pokemon2):
+    st.write(f"Battle between {pokemon1.name} and {pokemon2.name} begins!")
+    st.write(f"{pokemon1.name}: {pokemon1.health} HP")
+    st.write(f"{pokemon2.name}: {pokemon2.health} HP")
 
-def perform_action():
-# Action to be performed when the button is clicked
-    
-    if move_choice == 1:
-            faster_creature.attack(move1, slower_creature)
-    elif move_choice == 2:
-            faster_creature.attack(move2, faster_creature)
-    elif move_choice == 3:
-            faster_creature.attack(move3, slower_creature)
+    while pokemon1.health > 0 and pokemon2.health > 0:
+        attacker = random.choice([pokemon1, pokemon2])
+        defender = pokemon2 if attacker == pokemon1 else pokemon1
+
+        damage = random.randint(10, 20)
+        defender.take_damage(damage)
+
+        st.write(f"{attacker.name} attacked {defender.name} and dealt {damage} damage.")
+        st.write(f"{pokemon1.name}: {pokemon1.health} HP")
+        st.write(f"{pokemon2.name}: {pokemon2.health} HP")
+        st.write("---")
+
+    if pokemon1.health > 0:
+        st.write(f"{pokemon1.name} wins the battle!")
     else:
-        st.write("Invalid move choice. Try again.")
-
-    st.write(f"Creature 1 Health: {creature1.health}")
-    st.write(f"Creature 2 Health: {creature2.health}")
+        st.write(f"{pokemon2.name} wins the battle!")
 
 
-creature1 = Creature(10, 100, 20, 30, 15, 10, 5, 50)
-creature2 = Creature(8, 80, 15, 25, 12, 8, 3, 60)
+def main():
+    st.title("Pokemon Battle")
 
-move1 = Move("Fireball", 40, MoveType.Magic, False)
-move2 = Move("Heal", 30, MoveType.Magic, True)
-move3 = Move("Tackle", 20, MoveType.Physical, False)
+    pokemon1 = Pokemon("Pikachu", 100, 20)
+    pokemon2 = Pokemon("Charizard", 120, 18)
 
-while creature1.health > 0 and creature2.health > 0:
-    faster_creature = creature1 if creature1.speed > creature2.speed else creature2
-    slower_creature = creature2 if faster_creature == creature1 else creature1
+    st.write("Welcome to the Pokemon Battle!")
+    st.write(f"Today's battle is between {pokemon1.name} and {pokemon2.name}.")
 
-    st.write(f"\n{faster_creature}'s turn:")
-    st.write(f"1. {move1.name}")
-    st.write(f"2. {move2.name}")
-    st.write(f"3. {move3.name}")
+    if st.button("Start Battle"):
+        battle(pokemon1, pokemon2)
 
-    move_choice = st.number_input("Enter your move (1-3): ", min_value=1, max_value=3, value=1, step=1)
 
-    st.button('Go', on_click=perform_action)
-
-if creature1.health > 0:
-    st.write("Creature 1 wins!")
-else:
-    st.write("Creature 2 wins!")
+if __name__ == "__main__":
+    main()
