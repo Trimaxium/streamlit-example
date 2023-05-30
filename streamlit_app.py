1e1b0fc92fe54b24a6811b93e4dfac17ec4ce05c
Trimaxium <31966278+Trimaxium@users.noreply.github.com>
2023-05-29 21:45:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,6 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-import streamlit as st
-
-
 class Creature:
     def __init__(self, level, health, physical_attack, magic_attack, armor, magic_resistance, faith, speed):
         self.level = level
@@ -43,11 +40,11 @@ class Creature:
         if move.is_healing_move:
             healing_amount = self.calculate_healing(move)
             self.health += healing_amount
-            st.write(f"Creature healed for {healing_amount} health.")
+            print(f"Creature healed for {healing_amount} health.")
         else:
             damage_amount = self.calculate_damage(move, defender)
             defender.health -= damage_amount
-            st.write(f"Creature dealt {damage_amount} damage to the opponent.")
+            print(f"Creature dealt {damage_amount} damage to the opponent.")
 
 
 class Move:
@@ -63,44 +60,37 @@ class MoveType:
     Magic = 2
 
 
-def main():
-    creature1 = Creature(10, 100, 20, 30, 15, 10, 5, 50)
-    creature2 = Creature(8, 80, 15, 25, 12, 8, 3, 60)
+creature1 = Creature(10, 100, 20, 30, 15, 10, 5, 50)
+creature2 = Creature(8, 80, 15, 25, 12, 8, 3, 60)
 
-    move1 = Move("Fireball", 40, MoveType.Magic, False)
-    move2 = Move("Heal", 30, MoveType.Magic, True)
-    move3 = Move("Tackle", 20, MoveType.Physical, False)
+move1 = Move("Fireball", 40, MoveType.Magic, False)
+move2 = Move("Heal", 30, MoveType.Magic, True)
+move3 = Move("Tackle", 20, MoveType.Physical, False)
 
-    st.title("Pokemon Battle")
+while creature1.health > 0 and creature2.health > 0:
+    faster_creature = creature1 if creature1.speed > creature2.speed else creature2
+    slower_creature = creature2 if faster_creature == creature1 else creature1
 
-    while creature1.health > 0 and creature2.health > 0:
-        faster_creature = creature1 if creature1.speed > creature2.speed else creature2
-        slower_creature = creature2 if faster_creature == creature1 else creature1
+    st.write(f"\n{faster_creature}'s turn:")
+    st.write(f"1. {move1.name}")
+    st.write(f"2. {move2.name}")
+    st.write(f"3. {move3.name}")
 
-        st.write(f"\n{faster_creature}'s turn:")
-        st.write(f"1. {move1.name}")
-        st.write(f"2. {move2.name}")
-        st.write(f"3. {move3.name}")
+    move_choice = st.number_input("Enter your move (1-3): ")
 
-        move_choice = st.number_input("Enter your move (1-3): ", min_value=1, max_value=3, value=1, step=1)
-
-        if move_choice == 1:
-            faster_creature.attack(move1, slower_creature)
-        elif move_choice == 2:
-            faster_creature.attack(move2, faster_creature)
-        elif move_choice == 3:
-            faster_creature.attack(move3, slower_creature)
-        else:
-            st.write("Invalid move choice. Try again.")
-
-        st.write(f"Creature 1 Health: {creature1.health}")
-        st.write(f"Creature 2 Health: {creature2.health}")
-
-    if creature1.health > 0:
-        st.write("Creature 1 wins!")
+    if move_choice == 1:
+        faster_creature.attack(move1, slower_creature)
+    elif move_choice == 2:
+        faster_creature.attack(move2, faster_creature)
+    elif move_choice == 3:
+        faster_creature.attack(move3, slower_creature)
     else:
-        st.write("Creature 2 wins!")
+        st.write("Invalid move choice. Try again.")
 
+    st.write(f"Creature 1 Health: {creature1.health}")
+    st.write(f"Creature 2 Health: {creature2.health}")
 
-if __name__ == "__main__":
-    main()
+if creature1.health > 0:
+    st.write("Creature 1 wins!")
+else:
+    st.write("Creature 2 wins!")
